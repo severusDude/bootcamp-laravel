55176c087fd40a91a0fc0bb8bf50d8774e9a5cfb
severusDude <fajarsaladin05@gmail.com>
2024-07-21 17:21:34 +0700

    feat(middleware): register middleware to routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// standard routes
+Route::get('/news/{id}', function (string $id) {
+    return;
+});
+
+// admin routes
+Route::group(['middleware' => ['role:admin']], function () {
+    Route::any('/admin');
+});
+
+// editor routes
+Route::group(['middleware' => ['role:admin|editor']], function () {
+    return;
+});
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
