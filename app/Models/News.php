f4b0faa48037af597dc9fc88e637cfec98141165
severusDude <fajarsaladin05@gmail.com>
2024-07-21 23:57:33 +0700

    feat(news): add create comment function on current news

--- app/Models/News.php
+++ app/Models/News.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Auth;
 
 class News extends Model
 {
@@ -15,6 +16,20 @@ class News extends Model
 
     public function comments()
     {
-        $this->hasMany(Comment::class);
+        return $this->hasMany(Comment::class);
+    }
+
+    public function createComment($content)
+    {
+        // find current comment_id of current news
+        $current_id = $this->comments()->max('comment_id');
+        $new_id = $current_id ? $current_id + 1 : 1;
+
+        // add new comment
+        return $this->comments()->create([
+            'comment_id' => $new_id,
+            'created_by' => Auth::user()->id,
+            'content' => $content,
+        ]);
     }
 }
