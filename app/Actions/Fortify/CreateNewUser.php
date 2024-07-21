762ddcb373563010f61393052b6990c7d0d24581
severusDude <fajarsaladin05@gmail.com>
2024-07-21 07:24:47 +0700

    feat(register): apply standard role to every user register

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -26,10 +26,14 @@ class CreateNewUser implements CreatesNewUsers
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
 
-        return User::create([
+        $created_user = User::create([
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
         ]);
+
+        $created_user->assignRole('standard');
+
+        return $created_user;
     }
 }
