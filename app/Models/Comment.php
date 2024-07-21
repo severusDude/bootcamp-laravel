5d03d3966f7b1f6f89dd84603580de9a4b5fec76
severusDude <fajarsaladin05@gmail.com>
2024-07-22 00:32:24 +0700

    feat(comment): add automatic comment_id assignment unique on each news

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -14,9 +14,11 @@ class Comment extends Model
     protected $primaryKey = ['comment_id', 'news_id'];
     public $incrementing = false;
 
+    protected $fillable = ['content',];
+
     public function news()
     {
-        $this->belongsTo(News::class);
+        return $this->belongsTo(News::class);
     }
 
     protected function setKeysForSaveQuery($query)
@@ -24,5 +26,20 @@ class Comment extends Model
         $query->where('comment_id', '=', $this->getAttribute('comment_id'))->where('news_id', '=', $this->getAttribute('news_id'));
 
         return $query;
+    }
+
+    protected static function booted(): void
+    {
+        static::creating(function (Comment $comment) {
+            // get news id
+            $news_id = $comment->news_id;
+
+            // current news comment count
+            $current_id = self::where('news_id', $news_id)->count();
+            $new_id = $current_id ? $current_id + 1 : 1;
+
+            // set new_id for comment_id
+            $comment->comment_id = $new_id;
+        });
     }
 }
