7671a4299d3e161bb13ae72bc960e93f85ca1aab
severusDude <fajarsaladin05@gmail.com>
2024-07-21 22:41:22 +0700

    feat(comment): add comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -1,3 +1,4 @@
+
 <?php
 
 namespace App\Models;
@@ -6,15 +7,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class News extends Model
+class Comment extends Model
 {
     use HasFactory;
     use SoftDeletes;
-
-    protected $primaryKey = 'news_id';
-
-    public function comments()
-    {
-        $this->hasMany(Comment::class);
-    }
 }
---
+++ database/migrations/2024_07_21_152925_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\News;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('comment_id');
+            $table->foreignIdFor(News::class, 'news_id');
+            $table->foreignId('created_by')->constrained('users', 'id');
+            $table->timestamps();
+            $table->text('content');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
