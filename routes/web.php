0615cfd02ae0cbcf301602497b5a5156e77f6aa9
severusDude <fajarsaladin05@gmail.com>
2024-07-22 11:01:29 +0700

    feat(route): add admin routes
    index users and manage categories,

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,12 +9,28 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// standard routes
-Route::get('/news/{id}', [NewsController::class, 'read_news']);
+// basic routes
+Route::get('/{category}/{news_id}', [NewsController::class, 'read_news']);
 
 // admin routes
-Route::group(['middleware' => ['role:admin']], function () {
-    Route::any('/admin');
+Route::group(['middleware' => ['role:admin'], 'prefix' => '/admin'], function () {
+
+    // index users
+    Route::view('/users', 'admin.users');
+
+    // manage news
+    Route::controller(NewsController::class)->group(function () {
+        Route::get('/news', 'index_news');
+        Route::delete('/{category}/{news_id}', 'delete_news');
+    });
+
+    // manage categories (/admin/category)
+    Route::controller(CategoryController::class)->prefix('/category')->group(function () {
+        Route::get('/', 'index_category');
+        Route::post('/create', 'create_category');
+        Route::put('/{category}', 'edit_category');
+        Route::delete('/{category}', 'delete_category');
+    });
 });
 
 // editor routes
