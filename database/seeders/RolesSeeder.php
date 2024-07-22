15b7580dd406ab3496ac2c2cbc236ae387f21f57
severusDude <fajarsaladin05@gmail.com>
2024-07-22 12:02:50 +0700

    feat(permission): add for category and comment

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -20,16 +20,26 @@ class RolesSeeder extends Seeder
         $role_standard = Role::create(['name' => 'standard']);
 
         // create permissions
+        // category permissions
+        $category_create = Permission::create(['name' => 'create category']);
+        $category_update = Permission::create(['name' => 'update category']);
+        $category_delete = Permission::create(['name' => 'delete category']);
+
         // news permissions
-        $news_create = Permission::create(['name' => 'create_news']);
-        $news_read = Permission::create(['name' => 'read_news']);
-        $news_update = Permission::create(['name' => 'update_news']);
-        $news_delete = Permission::create(['name' => 'delete_news']);
+        $news_create = Permission::create(['name' => 'create news']);
+        $news_update = Permission::create(['name' => 'update news']);
+        $news_delete = Permission::create(['name' => 'delete news']);
+        $news_restore = Permission::create(['name' => 'restore news']);
+
+        // comment permissions
+        $comment_create = Permission::create(['name' => 'create comment']);
+        $comment_update = Permission::create(['name' => 'update comment']);
+        $comment_delete = Permission::create(['name' => 'delete comment']);
 
         // role permissions assignment
-        $permissions_admin = [$news_create, $news_read, $news_update, $news_delete];
-        $permissions_editor = [$news_create, $news_read, $news_update, $news_delete];
-        $permissions_standard = [$news_read];
+        $permissions_admin = [$category_create, $category_update, $category_delete, $news_delete, $news_restore];
+        $permissions_editor = [$news_create, $news_update, $news_delete];
+        $permissions_standard = [$comment_create, $comment_update, $comment_delete];
 
         $role_admin->syncPermissions($permissions_admin);
         $role_editor->syncPermissions($permissions_editor);
