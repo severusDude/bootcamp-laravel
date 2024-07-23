c74a3650c174fb597ec19606eec89248a347b411
severusDude <fajarsaladin05@gmail.com>
2024-07-24 00:36:41 +0700

    feat(comment): add create comment

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function create_comment()
+    public function create_comment(Request $request)
     {
-        return;
+        $uri = explode("/", $request->getUri());
+
+        $category_id = $uri[3];
+        $news_id = $uri[4];
+
+        $comment = new Comment;
+
+        $comment->news_id = $news_id;
+        $comment->content = $request->comment;
+        $comment->save();
+
+        return redirect('/');
     }
 
     public function edit_comment(
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -35,11 +35,12 @@
                             ({{ $news->comments->count() }})</h2>
                     </div>
                     @can('create comment')
-                        <form class="mb-6">
+                        <form method="POST" action="{{ url()->current() }}" class="mb-6">
+                            @csrf
                             <div
                                 class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
                                 <label for="comment" class="sr-only">Your comment</label>
-                                <textarea id="comment" rows="6"
+                                <textarea name="comment" id="comment" rows="6"
                                     class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
                                     placeholder="Write a comment..." required></textarea>
                             </div>
