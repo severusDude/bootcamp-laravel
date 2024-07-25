6ecfde309842750fba65c5b9a4b1218e65b22ca2
severusDude <fajarsaladin05@gmail.com>
2024-07-25 13:27:46 +0700

    feat(category): change category_name to name

--- database/migrations/2024_07_21_144326_create_categories_table.php
+++ database/migrations/2024_07_21_144326_create_categories_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('category_name')->unique();
-            $table->text('category_desc')->nullable();
+            $table->string('name')->unique();
+            $table->text('desc')->nullable();
             $table->timestamps();
         });
     }
