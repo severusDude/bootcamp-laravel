2115cf7c799731b7d22224ff1ab0821e1d739a47
severusDude <fajarsaladin05@gmail.com>
2024-07-25 13:56:14 +0700

    feat(category): add delete category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -78,8 +78,9 @@ class CategoryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return redirect()->route('admin.categories.index')->with('success', 'Category deleted successfully');
     }
 }
