e5e36568963c227fbe703e9d7daa167d183e757e
severusDude <fajarsaladin05@gmail.com>
2024-07-25 13:28:05 +0700

    feat(category): add index category

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -52,7 +52,7 @@
                 </button>
                 <ul x-show="open" x-transition class="mt-2 space-y-2">
                     <li>
-                        <a href="#"
+                        <a href="{{ route('admin.categories.index') }}"
                             class="flex items-center p-2 pl-11 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                             <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                                 aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
