8d94ffd063943318ecf04861119a084b9234a726
severusDude <fajarsaladin05@gmail.com>
2024-07-27 14:27:52 +0700

    fix(admin): remove settings and logout button from sidebar

--- resources/views/partials/admin/sidebar.blade.php
+++ resources/views/partials/admin/sidebar.blade.php
@@ -104,7 +104,7 @@
             </li>
 
             <!-- Settings -->
-            <li>
+            {{-- <li>
                 <a href="#"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -117,10 +117,10 @@
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap">Pengaturan</span>
                 </a>
-            </li>
+            </li> --}}
 
             <!-- Logout -->
-            <li>
+            {{-- <li>
                 <form method="POST" action="{{ route('logout') }}"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     @csrf
@@ -134,7 +134,7 @@
                         <span class="ml-3">Logout</span>
                     </button>
                 </form>
-            </li>
+            </li> --}}
 
         </ul>
     </div>
