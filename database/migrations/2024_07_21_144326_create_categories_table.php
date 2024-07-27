6fe8688f058571810bdffe4506a527daab58b9ce
severusDude <fajarsaladin05@gmail.com>
2024-07-27 13:51:35 +0700

    feat(category): add soft delete to model

--- database/migrations/2024_07_21_144326_create_categories_table.php
+++ database/migrations/2024_07_21_144326_create_categories_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name')->unique();
             $table->text('desc')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
